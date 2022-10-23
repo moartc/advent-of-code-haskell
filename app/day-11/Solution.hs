8a733b2cca7250d9b00c346240a6eeb5196627af
mo <allenszwester@gmail.com>
2022-10-23 21:12:42 +0200

    Fixed input parsing

--- app/day-11/Solution.hs
+++ app/day-11/Solution.hs
@@ -1,13 +1,8 @@
-import Data.List.Split
-import Control.Arrow (Arrow(first))
+import Data.List (isInfixOf)
+import Data.List.Split ( splitOn )
 
-
-input = "The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.\nThe second floor contains a hydrogen generator.\nThe third floor contains a lithium generator.\nThe fourth floor contains nothing relevant."
-
-str1="The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip."
-str2="The second floor contains a hydrogen generator."
-str3="The third floor contains a lithium generator."
-str4="The fourth floor contains nothing relevant."
+input :: [Char]
+input = "The first floor contains a strontium generator, a strontium-compatible microchip, a plutonium generator, and a plutonium-compatible microchip.\nThe second floor contains a thulium generator, a ruthenium generator, a ruthenium-compatible microchip, a curium generator, and a curium-compatible microchip.\nThe third floor contains a thulium-compatible microchip.\nThe fourth floor contains nothing relevant."
 
 parseOnePart :: String -> String
 parseOnePart s = firstPartAbb : [lastChar]
@@ -17,24 +12,22 @@ parseOnePart s = firstPartAbb : [lastChar]
     lastChar = head $ last splitted
 
 parseWholeLine :: String -> [String]
-parseWholeLine line = arr
+parseWholeLine line =
+  if "nothing relevant" `isInfixOf` line
+    then []
+    else map parseOnePart splitted
   where
     dropped = drop 27 line
     withoutBeginning = splitOn " and a " dropped
     splitted = concatMap (splitOn " a ") withoutBeginning
-    arr = map parseOnePart splitted
 
 parseInput :: String -> [[String]]
-parseInput = map parseWholeLine . init . lines 
+parseInput input = map parseWholeLine $ lines input
 
---parseInput :: String -> [[String]]
---parseInput s = dropWhile (a -> Bool) ([a])
-
---part1 :: String -> Int
---part1 input = takeWhile (a -> Bool) ([a])
-
+part1 :: String -> [String]
+part1 = lines
 
 main :: IO ()
 main = do
   input <- readFile "inputs/day-11.txt"
-  putStrLn "Part 1 = "
+  putStrLn $ "Part 1 = " ++ show (parseInput input)
