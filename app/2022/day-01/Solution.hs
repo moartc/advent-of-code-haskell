4b9bb16d25f7d79edb86178b39667161f8f449b1
mo <allenszwester@gmail.com>
2022-12-01 20:38:29 +0100

    Small refactoring - 2022/01

--- app/2022/day-01/Solution.hs
+++ app/2022/day-01/Solution.hs
@@ -1,20 +1,9 @@
 import Data.List ( sort )
-
-inpToList :: [String] ->Int-> [Int] -> [Int]
-inpToList (x:xs) prev list
-  | x == "" = inpToList xs 0 list ++ [prev]
-  | otherwise = inpToList xs (prev + (read x :: Int)) list    
-inpToList [] _ list = list
-
-part1 :: [String] -> Int
-part1 inp = maximum $ inpToList inp 0 [] 
-
-part2 :: [String] -> Int
-part2 inp = sum $ take 3 $ reverse $ sort $ inpToList inp 0 []   
-    
+import Data.List.Split (splitOn)
 
 main :: IO ()
 main = do
-  input <- readFile "inputs/2022/day-01.txt"  
-  putStrLn $ "Part 1 = " ++ show(part1 $ lines input)
-  putStrLn $ "Part 2 = " ++ show (part2 $ lines input)
+  input <- readFile "inputs/2022/day-01.txt"
+  let sumArr = [sum (map (\ x -> (read x :: Int)) list) | list <- splitOn [""] $ lines input ] 
+  putStrLn $ "Part 1 = " ++ show(maximum sumArr)
+  putStrLn $ "Part 2 = " ++ show (sum $ take 3 $ reverse $ sort sumArr)
