8576ee92de1fea7f4db10aa5348490be0be597a7
mo <allenszwester@gmail.com>
2022-12-11 12:51:53 +0100

    Day 2022/11 - part 2

--- app/2022/day-11/Solution.hs
+++ app/2022/day-11/Solution.hs
@@ -1,11 +1,6 @@
 import Data.Char (digitToInt, isDigit)
-import Data.Either (rights)
 import Data.List (sort)
 import Data.List.Split (splitOn)
-import Data.Monoid (First (First))
-import Debug.Trace
-
-debug = flip trace
 
 data Monkey = Monkey {items :: [Int], op :: Char, opRightSide :: Either Int String, divisibleBy :: Int, ifTrue :: Int, ifFalse :: Int} deriving (Show)
 
@@ -15,45 +10,43 @@ addItem m i = m {items = items m ++ [i]}
 resetItems :: Monkey -> Monkey
 resetItems m = m {items = []}
 
-inp = "Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n\nMonkey 1:\n  Starting items: 54, 65, 75, 74\n  Operation: new = old + 6\n  Test: divisible by 19\n    If true: throw to monkey 2\n    If false: throw to monkey 0\n\nMonkey 2:\n  Starting items: 79, 60, 97\n  Operation: new = old * old\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 3\n\nMonkey 3:\n  Starting items: 74\n  Operation: new = old + 3\n  Test: divisible by 17\n    If true: throw to monkey 0\n    If false: throw to monkey 1\n"
-
-t1 = parseInput inp
-
 newVal :: Int -> Char -> Either Int String -> Int
 newVal int char (Left newVal) = if char == '*' then int * newVal else int + newVal
 newVal int char (Right _) = if char == '*' then int * int else int + int
 
 type RoundState = ([Int], [Monkey])
 
-oneMonkeyRound :: RoundState -> Int -> RoundState
-oneMonkeyRound (insp, ms) monkeyIdx = (updatedInsp, fst x)
+oneMonkeyRound :: RoundState -> Int -> Bool -> RoundState
+oneMonkeyRound (insp, ms) monkeyIdx divBy3 = (updatedInsp, fst newState)
   where
     currentMonkeyInsp = insp !! monkeyIdx
-    x = monkeyRound ms monkeyIdx
-    updatedInsp = replace monkeyIdx (currentMonkeyInsp + snd x) insp
+    newState = monkeyRound ms monkeyIdx divBy3
+    updatedInsp = replace monkeyIdx (currentMonkeyInsp + snd newState) insp
 
 part1 :: String -> Int
 part1 inp = product $ take 2 $ reverse $ sort $ fst finalRS
   where
     monkeys = parseInput inp
     initRoundState = (replicate (length monkeys) 0, monkeys)
-    finalRS = foldl (\x y -> allMonkeysRound x 0) initRoundState [1 .. 20]
-    
+    finalRS = foldl (\x y -> allMonkeysRound x 0 True) initRoundState [1 .. 20]
 
-allMonkeysRound :: RoundState -> Int -> RoundState
-allMonkeysRound rs idx
-  | idx == ms = rs
-  | otherwise = allMonkeysRound newState (idx + 1)
+part2 :: String -> Int
+part2 inp = product $ take 2 $ reverse $ sort $ fst finalRS
   where
-    ms = length $ snd rs
-    newState = oneMonkeyRound rs idx
+    monkeys = parseInput inp
+    initRoundState = (replicate (length monkeys) 0, monkeys)
+    finalRS = foldl (\x y -> allMonkeysRound x 0 False) initRoundState [1 .. 10000]
 
-monkeyRound :: [Monkey] -> Int -> ([Monkey], Int)
-monkeyRound monkeys idx = (replace idx resetMonk firstUpdate, length $ items monk) -- `debug` ("return " ++ show (inspToRet))
+allMonkeysRound :: RoundState -> Int -> Bool -> RoundState
+allMonkeysRound rs idx divBy3 = foldl (\r y -> oneMonkeyRound r y divBy3) rs [0 .. length (snd rs) -1]
+
+monkeyRound :: [Monkey] -> Int -> Bool -> ([Monkey], Int)
+monkeyRound monkeys idx divBy3 = (replace idx resetMonk firstUpdate, length $ items monk) -- `debug` ("return " ++ show (inspToRet))
   where
-    monk = monkeys !! idx    
-    firstUpdate = updateMonkeys monkeys [handleItem monk item | item <- items monk]  
-    resetMonk = resetItems monk    
+    monk = monkeys !! idx
+    firstUpdate = updateMonkeys monkeys [handleItem monk item allDivs divBy3 | item <- items monk]
+    resetMonk = resetItems monk
+    allDivs = product $ map divisibleBy monkeys
 
 updateMonkeys :: [Monkey] -> [(Int, Int)] -> [Monkey]
 updateMonkeys all ((toReplace, newValue) : us) = updateMonkeys replaced us
@@ -63,11 +56,11 @@ updateMonkeys all ((toReplace, newValue) : us) = updateMonkeys replaced us
     replaced = replace toReplace updated all
 updateMonkeys all [] = all
 
-handleItem :: Monkey -> Int -> (Int, Int)
-handleItem monkey itemVal = (throwTo, valToThrow)
+handleItem :: Monkey -> Int -> Int -> Bool -> (Int, Int)
+handleItem monkey itemVal allDivs divBy3 = (throwTo, valToThrow)
   where
     newValue = newVal itemVal (op monkey) (opRightSide monkey)
-    valToThrow = newValue `div` 3
+    valToThrow = newValue `mod` allDivs `div` (if divBy3 then 3 else 1)
     throwTo = if valToThrow `mod` divisibleBy monkey == 0 then ifTrue monkey else ifFalse monkey
 
 parseInput :: String -> [Monkey]
@@ -89,7 +82,7 @@ main :: IO ()
 main = do
   input <- readFile "inputs/2022/day-11.txt"
   putStrLn $ "Part 1 = " ++ show (part1 input)
-  putStrLn $ "Part 2 = " -- ++ show (part1 input)
+  putStrLn $ "Part 2 = " ++ show (part2 input)
 
 replace :: Int -> a -> [a] -> [a]
 replace pos newVal list = take pos list ++ newVal : drop (pos + 1) list
